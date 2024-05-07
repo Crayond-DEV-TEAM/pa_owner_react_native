e8abc52cd5c464247916e60d70ab80a9e6de91cc
Manojkumar <manojkumar@crayond.co>
2024-05-07 13:15:24 +0530

    Camera permission access changes

--- App.jsx
+++ App.jsx
@@ -11,15 +11,21 @@ import React from 'react';
 import WebScreen from './src/screen/webScreen';
 import SplashScreen from 'react-native-splash-screen';
 import {Platform} from 'react-native';
+import {requestCameraPermission} from './src/utils/accessPermissions';
 
 const App = () => {
   React.useEffect(() => {
+    permission();
     if (Platform.OS === 'android') {
       // eslint-disable-next-line no-undef
       SplashScreen.hide();
     }
     // getVersion();
   }, []);
+
+  const permission = async () => {
+    await requestCameraPermission();
+  };
 
   return <WebScreen />;
 };
