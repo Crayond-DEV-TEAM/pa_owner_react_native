30d78fac296858024445636855cf4f33f77e9b8a
Manojkumar <manojkumar@crayond.co>
2024-09-12 15:12:13 +0530

    Android V1.0.3(5) & iOS V1.0.7(1)

--- src/screen/webScreen.jsx
+++ src/screen/webScreen.jsx
@@ -1,33 +1,70 @@
-import React from 'react';
-
-import {SafeAreaView, StatusBar, StyleSheet, Linking} from 'react-native';
-
+import React, {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Linking,
+  ActivityIndicator,
+} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {WebView} from 'react-native-webview';
 
-const INJECTED_JAVASCRIPT = `(function() {
-    const authLocalStorage = window.localStorage.getItem('auth_token');
+const getInjectedJavascript = token => `(function () {
+  const authLocalStorage = '${token}' || window.localStorage.getItem('auth_token');
 
-    const obj = {
-        authLocalStorage,
-    }
+  const obj = {
+    authLocalStorage,
+  };
 
-    const getItemLocalStorage = JSON.stringify(obj);
-    window.ReactNativeWebView.postMessage(getItemLocalStorage);
-})();`;
+  const getItemLocalStorage = JSON.stringify(obj);
+  window.ReactNativeWebView.postMessage(getItemLocalStorage);
+})()`;
 
 const WebScreen = props => {
-  const {diviceToken} = props;
+  const [authToken, setAuthToken] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const onMessage = payload => {
-    console.log('payload asses', payload);
+  useEffect(() => {
+    const fetchAuthToken = async () => {
+      try {
+        const token = await AsyncStorage.getItem('auth_token');
+        setAuthToken(token);
+      } catch (error) {
+        console.error('Error fetching auth token:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAuthToken();
+  }, []);
+
+  const onMessage = async event => {
+    const {authLocalStorage} = JSON.parse(event.nativeEvent.data);
+    if (authLocalStorage !== undefined) {
+      try {
+        await AsyncStorage.setItem('auth_token', authLocalStorage);
+      } catch (error) {
+        console.error('Error saving auth token:', error);
+      }
+    }
   };
 
   const WebviewRender = () => {
+    if (loading) {
+      return (
+        <SafeAreaView style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </SafeAreaView>
+      );
+    }
+
     return (
       <WebView
-        injectedJavaScript={INJECTED_JAVASCRIPT}
+        ref={ref => (this.webviewRef = ref)}
+        injectedJavaScript={getInjectedJavascript(authToken)}
         onMessage={onMessage}
-        source={{uri: `https://my.thabatre.sa/`}}
+        source={{uri: 'https://my.thabatre.sa/'}}
         onShouldStartLoadWithRequest={event => {
           const {url} = event;
           if (url.startsWith('tel:')) {
@@ -57,5 +94,5 @@ const styles = StyleSheet.create({
     marginTop: -StatusBar.currentHeight + 10,
     justifyContent: 'center',
     backgroundColor: 'white',
-  }
+  },
 });
